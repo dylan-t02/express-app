89c47ac79921777f627ab4063575a4a6c588d2d7
Dylan Thomas <thommyboy02@gmail.com>
2016-12-27 12:22:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Order
+
+### npm init => npm i -S express => npm i -S express-handlebars
+
+### require express and listen on port 3000.
---
+++ express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", function(req, res) {
+    res.send("Express works");
+});
+
+app.listen(app.get("port"), function(){
+    console.log("Express working");
+});
