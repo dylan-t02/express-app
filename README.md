a2e6e062207f19ac9e5ad54367bab2d3f4b0386b
Dylan Thomas <thommyboy02@gmail.com>
2016-12-27 19:27:01 +1100

    Bootstrap nav added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Order
+# README
 
 ### npm init => npm i -S express => npm i -S express-handlebars
 
--- express.js
+++ express.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.disable("x-powered-by");
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set("port", process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
-    res.send("Express works");
+    res.render("home");
 });
+
 
 app.listen(app.get("port"), function(){
     console.log("Express working");
